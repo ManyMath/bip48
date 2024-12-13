1b7d1bd18e059d4eeaf44f827427a9248d867f7b
sneurlax <sneurlax@gmail.com>
2024-12-12 19:15:44 -0600

    bip45 -> bip48

--- lib/bip48.dart
+++ lib/bip48.dart
@@ -3,6 +3,6 @@
 /// More dartdocs go here.
 library;
 
-export 'src/bip45_base.dart';
+export 'src/bip48_base.dart';
 
 // TODO: Export any libraries intended for clients of this package.
--- test/bip48_test.dart
+++ test/bip48_test.dart
@@ -1,4 +1,4 @@
-import 'package:bip45/bip45.dart';
+import 'package:bip48/bip48.dart';
 import 'package:test/test.dart';
 
 void main() {
