13b476c4ed72ac47a909386bb013e916b508b011
sneurlax <sneurlax@gmail.com>
2024-12-23 22:08:13 -0600

    feat: add secp256k1 helper script

---
+++ scripts/linux/build_secp256k1.sh
@@ -0,0 +1,18 @@
+# This script is provided in case `dart run coinlib:build_linux` fails.
+#
+# Adapted from Stack Wallet's secp256k1 build script.
+
+mkdir -p build
+cd build
+if [ ! -d "secp256k1" ]; then
+    git clone https://github.com/bitcoin-core/secp256k1
+fi
+cd secp256k1
+git checkout 68b55209f1ba3e6c0417789598f5f75649e9c14c
+git reset --hard
+mkdir -p build && cd build
+cmake ..
+cmake --build .
+echo "$PWD"
+cp lib/libsecp256k1.so.2.2.2 "../../libsecp256k1.so"
+cd ../../
